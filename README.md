c4cbf5fa946c2277683e7c43fe6b436aa3d634a4
Emmy Le <emilytuyetle@gmail.com>
2021-05-01 22:52:45 -0700

    FIrst commit

---
+++ README.md
@@ -0,0 +1,14 @@
+##### This is a practice using Express
+
+/posts
+creating posts POST /posts
+fetching all posts GET/posts
+fetch a single post GET/posts/id
+update a single post PUT /users/id
+deleting a single user DELETE /users/id
+
+CRUD operations
+create
+read
+update
+delete"# Express_App" 
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const posts = require('./posts.json');
+const fs = require('fs')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function (req,res){
+    res.send('Hello world!');
+})
+
+app.get('/books', function(req, res){
+    res.send('There are 4 books in store');
+})
+
+app.post('/', function(req,res){
+    res.send('this is a post request')
+})
+
+app.get('/posts', (req,res)=>{
+    // fetch all posts
+    // send the posts array as response to the client
+    return res.json({ posts })
+})
+//create new user
+app.post('/posts',(req,res)=>{
+    console.log(req.body.newPost)
+ // create a new post from cient's request   
+ //save new post to existing database
+ posts.push(req.body.newPost);
+  //save updated data to posts.json
+ console.log({ posts })
+// stringify the json data
+let stringedData =JSON.stringify(posts, null, 2);
+fs.writeFile('posts.json', stringedData, function(err){
+    if (err){
+        return res.status(500).json({message: err})
+    }
+})
+// fetch single post
+app.get('/posts/:id',(req, res)=>{
+    //fetch req.params.id
+    let id = req.params.id;
+    //find user wth id
+   let foundPost = posts.find(post =>{
+       return String(post.id) === id;
+    })
+    if(foundPost){
+        return res.status(200).json({post:foundPost})   
+    }else{
+        return res.status(404),json({ message: "user not found"})
+    }
+})
+console.log(foundPost)
+
+//rewrite the file posts.json
+ //send back a response to client
+ return res.status(200).json({message: 'new post created'})
+})
+app.listen(3000, function(){
+    console.log('server is up and running')
+})
